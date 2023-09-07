693e3d31b07823caa8e7b2b47f4e426b919f70d8
DanielElix <info@dreamscythe.com>
2023-09-07 13:30:21 +0200

    db connect
    add connection

--- connect.php
+++ connect.php
@@ -1,5 +1,10 @@
 <?php
 
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "aiai";
 
+$connection = mysqli_connect($host,$username,$password,$db) OR die('could not connect because:'. mysqli_connect_error());
 
- ?>
+?>
